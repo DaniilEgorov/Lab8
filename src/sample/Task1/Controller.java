90360beaf521bd956ca1b7a044bec1b0f7e20e8a
danil <dan20002100@mail.ru>
2019-11-09 16:47:34 +0300

    work

--- src/sample/Task1/Controller.java
+++ src/sample/Task1/Controller.java
@@ -8,56 +8,51 @@ import javafx.scene.paint.Color;
 
 public class Controller {
     @FXML
-    AnchorPane pane ;
+    AnchorPane pane;
+
     @FXML
-    void figure(){
-        double f = Math.floor(Math.random()*4);
-        Canvas canvas = new Canvas(500,350);
+    void figure() {
+        double f = Math.floor(Math.random() * 4);
+        Canvas canvas = new Canvas(500, 350);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        if(f==0){
+        if (f == 0) {
             gc.setStroke(Color.BLUE);
             gc.setLineWidth(1);
-            gc.strokeLine(Math.random()*500,Math.random()*350,Math.random()*500,Math.random()*350);
-        }
-        else if(f==1){
+            gc.strokeLine(Math.random() * 500, Math.random() * 350, Math.random() * 500, Math.random() * 350);
+        } else if (f == 1) {
             double k = Math.round(Math.random());
-            double a = Math.random()*500;
-            double b = Math.random()*350;
-            if(k==0){
+            double a = Math.random() * 500;
+            double b = Math.random() * 350;
+            if (k == 0) {
                 gc.setFill(Color.RED);
-                gc.fillOval(Math.random()*(500-a),Math.random()*(350-b),a,b);
-            }
-            else{
+                gc.fillOval(Math.random() * (500 - a), Math.random() * (350 - b), a, b);
+            } else {
                 gc.setStroke(Color.RED);
-                gc.strokeOval(Math.random()*(500-a),Math.random()*(350-b),a,b);
+                gc.strokeOval(Math.random() * (500 - a), Math.random() * (350 - b), a, b);
             }
-        }
-        else if(f==2){
+        } else if (f == 2) {
             double k = Math.round(Math.random());
-            double a = Math.random()*500;
-            double b = Math.random()*350;
-            if(k==0){
+            double a = Math.random() * 500;
+            double b = Math.random() * 350;
+            if (k == 0) {
                 gc.setFill(Color.GREEN);
-                gc.fillRect(Math.random()*(500-a),Math.random()*(350-b),a,b);
-            }
-            else{
+                gc.fillRect(Math.random() * (500 - a), Math.random() * (350 - b), a, b);
+            } else {
                 gc.setStroke(Color.GREEN);
-                gc.strokeRect(Math.random()*(500-a),Math.random()*(350-b),a,b);
+                gc.strokeRect(Math.random() * (500 - a), Math.random() * (350 - b), a, b);
             }
-        }
-        else {
+        } else {
             double k = Math.round(Math.random());
-            if(k==0){
+            if (k == 0) {
                 gc.setFill(Color.YELLOW);
                 gc.fillPolygon(
-                        new double[]{Math.random()*500,Math.random()*500,Math.random()*500},
-                        new double[]{Math.random()*350,Math.random()*350,Math.random()*500},3);
-            }
-            else {
+                        new double[]{Math.random() * 500, Math.random() * 500, Math.random() * 500},
+                        new double[]{Math.random() * 350, Math.random() * 350, Math.random() * 500}, 3);
+            } else {
                 gc.setStroke(Color.YELLOW);
                 gc.strokePolygon(
-                        new double[]{Math.random()*500,Math.random()*500,Math.random()*500},
-                        new double[]{Math.random()*350,Math.random()*350,Math.random()*350},3);
+                        new double[]{Math.random() * 500, Math.random() * 500, Math.random() * 500},
+                        new double[]{Math.random() * 350, Math.random() * 350, Math.random() * 350}, 3);
             }
         }
         pane.getChildren().add(canvas);
--- src/sample/Task2/Controller.java
+++ src/sample/Task2/Controller.java
@@ -10,88 +10,89 @@ import javafx.scene.paint.Color;
 
 public class Controller {
     @FXML
-    TextField x1,y1,x2,y2,
-              x3,y3,w,h,
-              x4,y4,a,b,
-              x5,y5,x6,y6,x7,y7;
+    TextField x1, y1, x2, y2,
+            x3, y3, w, h,
+            x4, y4, a, b,
+            x5, y5, x6, y6, x7, y7;
     @FXML
-    AnchorPane line,ellipse,rect,triangle;
+    AnchorPane line, ellipse, rect, triangle;
     @FXML
     CheckBox checkEllipse, checkRect, checkTriangle;
+
     @FXML
-    void drawLine(){
-        Canvas canvas = new Canvas(450,340);
+    void drawLine() {
+        Canvas canvas = new Canvas(450, 340);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.setStroke(Color.BLUE);
-        double x1,x2,y1,y2;
+        double x1, x2, y1, y2;
         x1 = Double.parseDouble(this.x1.getText());
         x2 = Double.parseDouble(this.x2.getText());
         y1 = Double.parseDouble(this.y1.getText());
         y2 = Double.parseDouble(this.y2.getText());
-        gc.strokeLine(x1,y1,x2,y2);
-        line.getChildren().remove(line.getChildren().size()-1);
+        gc.strokeLine(x1, y1, x2, y2);
+        line.getChildren().remove(line.getChildren().size() - 1);
         line.getChildren().add(canvas);
     }
+
     @FXML
-    void drawRect(){
-        Canvas canvas = new Canvas(450,340);
+    void drawRect() {
+        Canvas canvas = new Canvas(450, 340);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        double x,y,w,h;
+        double x, y, w, h;
         x = Double.parseDouble(this.x3.getText());
         y = Double.parseDouble(this.y3.getText());
         w = Double.parseDouble(this.w.getText());
         h = Double.parseDouble(this.h.getText());
-        if(checkRect.isSelected()){
+        if (checkRect.isSelected()) {
             gc.setFill(Color.RED);
-            gc.fillRect(x,y,w,h);
-        }
-        else {
+            gc.fillRect(x, y, w, h);
+        } else {
             gc.setStroke(Color.RED);
-            gc.strokeRect(x,y,w,h);
+            gc.strokeRect(x, y, w, h);
         }
-        rect.getChildren().remove(rect.getChildren().size()-1);
+        rect.getChildren().remove(rect.getChildren().size() - 1);
         rect.getChildren().add(canvas);
     }
+
     @FXML
-    void drawEllipse(){
-        Canvas canvas = new Canvas(450,340);
+    void drawEllipse() {
+        Canvas canvas = new Canvas(450, 340);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        double x,y,a,b;
+        double x, y, a, b;
         x = Double.parseDouble(this.x4.getText());
         y = Double.parseDouble(this.y4.getText());
         a = Double.parseDouble(this.a.getText());
         b = Double.parseDouble(this.b.getText());
-        if(checkEllipse.isSelected()){
+        if (checkEllipse.isSelected()) {
             gc.setFill(Color.GREEN);
-            gc.fillOval(x-a/2,y-b/2,a,b);
-        }
-        else {
+            gc.fillOval(x - a / 2, y - b / 2, a, b);
+        } else {
             gc.setStroke(Color.GREEN);
-            gc.strokeOval(x-a/2,y-b/2,a,b);
+            gc.strokeOval(x - a / 2, y - b / 2, a, b);
         }
-        ellipse.getChildren().remove(ellipse.getChildren().size()-1);
+        ellipse.getChildren().remove(ellipse.getChildren().size() - 1);
         ellipse.getChildren().add(canvas);
     }
+
     @FXML
-    void drawTriangle(){
-        Canvas canvas = new Canvas(450,340);
+    void drawTriangle() {
+        Canvas canvas = new Canvas(450, 340);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        double[] x = new double[3],y = new double[3];
+        double[] x = new double[3], y = new double[3];
         x[0] = Double.parseDouble(this.x5.getText());
         x[1] = Double.parseDouble(this.x6.getText());
         x[2] = Double.parseDouble(this.x7.getText());
         y[0] = Double.parseDouble(this.y5.getText());
         y[1] = Double.parseDouble(this.y6.getText());
         y[2] = Double.parseDouble(this.y7.getText());
-        if(checkTriangle.isSelected()){
+        if (checkTriangle.isSelected()) {
             gc.setFill(Color.YELLOW);
-            gc.fillPolygon(x,y,3);
-        }
-        else {
+            gc.fillPolygon(x, y, 3);
+        } else {
             gc.setStroke(Color.YELLOW);
-            gc.strokePolygon(x,y,3);
+            gc.strokePolygon(x, y, 3);
         }
-        triangle.getChildren().remove(triangle.getChildren().size()-1);
+        triangle.getChildren().remove(triangle.getChildren().size() - 1);
         triangle.getChildren().add(canvas);
     }
 }
